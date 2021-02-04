2edea0b327e70fa7f1f498fe42d34efb5fd78d92
alexis zaffalon <alexiszaffalon@live.fr>
2021-02-04 20:28:49 +0100

    Event crud + form for create & event card unfinished

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
-  belongs_to :hobby
+  # belongs_to :hobby
   has_many :messages, dependent: :destroy
   has_many :bookings, dependent: :destroy
 end
