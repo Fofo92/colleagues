fe4fde5ab81cadfe25904b0e4934e9132de30e9f
Pascal Fodiman <pascal@fodiman.fr>
2021-02-06 15:30:58 +0100

    add, delete & show event from User show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :events, only: [:index, :show, :edit, :update, :delete]
+  resources :events, only: [:index, :show, :edit, :update, :destroy]
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show, :edit, :update] do
     resources :events, only: [:new, :create]
   end
 end
