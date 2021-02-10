e962b14a2095097f5aadca49d6b3ad658ffce3ed
alexis zaffalon <alexiszaffalon@live.fr>
2021-02-10 16:15:54 +0100

    .

--- app/javascript/plugins/menu.js
+++ app/javascript/plugins/menu.js
@@ -1,43 +1,38 @@
 // Work
-const initMenu = () => {
-  const menuEls = document.querySelectorAll('.navbar .slide-button');
-  const bodyContainer = document.querySelector('.body-container');
-  const menuContainer = document.querySelector('.menu-container');
-  const menuContent = menuContainer.querySelector('.menu-container-content');
-  console.log(menuEls)
-  menuEls.forEach( el => {
-    el.addEventListener('click', () => {
-      event.preventDefault()
-      const timeout = menuContent.classList.contains('d-none') ? 0 : 400
-      setTimeout(() => {
-        menuContent.classList.toggle('d-none');
-      }, timeout)
-      bodyContainer.classList.toggle('width70');
-      menuContainer.classList.toggle('width30');
-      console.log(el)
-    });
-  });
-}
-
-//Try to make it work!
-
 // const initMenu = () => {
-//   const buttons = document.querySelectorAll('.navbar .nav-link');
-//   const testContainer = document.querySelector('.test-container');
+//   const menuEls = document.querySelectorAll('.navbar .slide-button');
+//   const bodyContainer = document.querySelector('.body-container');
 //   const menuContainer = document.querySelector('.menu-container');
 //   const menuContent = menuContainer.querySelector('.menu-container-content');
-//   buttons.forEach( el => {
-//     el.addEventListener('click', (event) => {
+//   console.log(menuEls)
+//   menuEls.forEach( el => {
+//     el.addEventListener('click', () => {
 //       event.preventDefault()
 //       const timeout = menuContent.classList.contains('d-none') ? 0 : 400
 //       setTimeout(() => {
 //         menuContent.classList.toggle('d-none');
 //       }, timeout)
-
-//       testContainer.classList.toggle('width70');
+//       bodyContainer.classList.toggle('width70');
 //       menuContainer.classList.toggle('width30');
+//       console.log(el)
 //     });
 //   });
 // }
+
+//Try to make it work!
+
+const initMenu = () => {
+  const buttons = document.querySelectorAll('.navbar .slide-button');
+  const bodyContainer = document.querySelector('.body-container');
+  const menuContainer = document.querySelector('.menu-container');
+  const menuContent = menuContainer.querySelector('.menu-container-content');
+  buttons.forEach( el => {
+    el.addEventListener('click', (event) => {
+      event.preventDefault()
+      bodyContainer.classList.toggle('menu-open');
+      menuContainer.classList.toggle('menu-open');
+    });
+  });
+}
 
 export { initMenu };
