aa67ce2757a6f6ab809b40679c630c4fdc4043fc
Pascal Fodiman <pascal@fodiman.fr>
2021-02-13 12:31:42 +0100

    add event types with form to user (multiple input)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  attr_accessor :event_types
+
   belongs_to :user
   has_one_attached :photo, dependent: :destroy
   has_many :messages, dependent: :destroy
@@ -9,6 +11,8 @@ class Event < ApplicationRecord
   #          inclusion: { in: (Date.today..Date.today + 1.years) }
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
+  EVENT_TYPES = ["Arts martiaux", "Rugby", "Autres sports", "Cinéma", "Théatre", "Littérature" "Football", "Concerts", "Week-ends",
+    "Afterworks", "Sorties, ballades", "Détente", "Conférences, cours"]
 
   private
 
