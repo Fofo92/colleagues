6935da9cbdc2b62d241b3b2e744c077973d29c93
Antoine Ung Bao <61555263+antoineub@users.noreply.github.com>
2021-02-26 23:05:36 +0100

    added surpriz (#83)
    Co-authored-by: Antoine Ung Bao <antoineungbao@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/team', to: 'pages#team'
 
   resources :events, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
     resources :bookings, only: [:index, :create]
