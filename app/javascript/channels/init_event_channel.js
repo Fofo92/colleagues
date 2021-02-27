f87f2649d4be5953436b9932b4243a4dc5b9a81a
alexis zaffalon <alexiszaffalon@live.fr>
2021-02-27 10:03:36 +0100

    add js for chatroom

--- app/javascript/channels/init_event_channel.js
+++ app/javascript/channels/init_event_channel.js
@@ -3,13 +3,15 @@ import consumer from "./consumer";
 const initEventCable = () => {
   const messagesContainer = document.getElementById('messages');
   if (messagesContainer) {
-    const id = messagesContainer.dataset.eventId;
+    const id = messagesContainer.dataset.eventId
+    const userId = messagesContainer.dataset.userId
+    console.log(userId)
     consumer.subscriptions.create({ channel: "EventChannel", id: id }, {
       received(data) {
-        console.log(data); // called when data is broadcast in the cable
-        console.log(messagesContainer);
-        messagesContainer.insertAdjacentHTML('beforeend', data);
-        document.getElementById("message_content").value = "";
+        const authorId = data.match(/data-author-id="(\d)"/)[1]
+        if (authorId != userId) data = data.replace("current_user", "")
+        messagesContainer.insertAdjacentHTML('beforeend', data)
+        document.getElementById("message_content").value = ""
       },
     });
   }
